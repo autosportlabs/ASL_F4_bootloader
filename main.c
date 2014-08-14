9efb16085a611529c80ffe98a2bbf644fbdddf60
Brent Picasso <brent@autosportlabs.com>
2014-08-13 20:13:59 -0700

    barebones NOOP bootloader seems to work

--- main.c
+++ main.c
@@ -9,7 +9,46 @@
 #endif
 
 #include <stdio.h>
+#include "core_cm4.h"
+#define FLASH_USER_OFFSET 		0x08020000
+#define FLASH_USER_BASE         ((uint32_t)(0x08000000 + 0x20000))
 
+//-----------------------------------------------------------------------------
+// Code stolen from "matis"
+// http://forum.chibios.org/phpbb/viewtopic.php?f=2&t=338
+static void jumpToApp(uint32_t address)
+{
+	typedef void (*pFunction)(void);
+
+	pFunction Jump_To_Application;
+
+	// variable that will be loaded with the start address of the application
+	vu32* JumpAddress;
+	const vu32* ApplicationAddress = (vu32*) address;
+
+	// get jump address from application vector table
+	JumpAddress = (vu32*) ApplicationAddress[1];
+
+	// load this address into function pointer
+	Jump_To_Application = (pFunction) JumpAddress;
+
+	// reset all interrupts to default
+//	chSysDisable();
+
+	// Clear pending interrupts just to be on the save side
+	//SCB_ICSR = ICSR_PENDSVCLR;
+
+	// Disable all interrupts
+	int i;
+	for (i = 0; i < 8; i++)
+		NVIC->ICER[i] = NVIC->IABR[i];
+
+	NVIC_SetVectorTable(NVIC_VectTab_FLASH, 0x20000);
+
+	// set stack pointer as in application's vector table
+	__set_MSP((u32)(ApplicationAddress[0]));
+	Jump_To_Application();
+}
 
 int main(void)
 {
@@ -19,6 +58,6 @@ int main(void)
 	printf("STM32F4xx Blinkenlights Demo\n");
 #endif
 
-
+	jumpToApp(FLASH_USER_BASE);
 	while(1);
 }
