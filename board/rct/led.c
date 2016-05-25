a5f24cdcbc444aa18beeea63d53468f48eb698a5
Jeff Ciesielski <jeffciesielski@gmail.com>
2016-05-25 07:53:31 -0400

    Invert led active state

--- board/rct/led.c
+++ board/rct/led.c
@@ -50,9 +50,9 @@ void led_toggle(void)
 	const uint8_t led_state = GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_1);
 
 	if (led_state) {
-		GPIO_ResetBits(GPIOA, GPIO_Pin_1);
-	} else {
 		GPIO_SetBits(GPIOA, GPIO_Pin_1);
+	} else {
+		GPIO_ResetBits(GPIOA, GPIO_Pin_1);
 	}
 }
 
