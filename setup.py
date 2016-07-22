896d54d62c74ffa594734d0b7c8fbf25c6e87f06
Jeff Ciesielski <jeffciesielski@gmail.com>
2016-07-22 09:07:27 -0400

    Add version reporting and flash bound checking
    This prevents the bootloader from attempting to overwrite itself, and
    gives the app a way to report which bootloader version is present

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import os
 from setuptools import setup
+import re
 
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
@@ -8,9 +9,26 @@ from setuptools import setup
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+def extract_version():
+    with open('version.h') as inf:
+        lines = inf.readlines()
+
+    ver_map = {'MAJOR': 0,
+               'MINOR': 0,
+               'BUGFIX': 0}
+    for l in lines:
+        match = re.match(r'#define VER_(MAJOR|MINOR|BUGFIX) (\d+)', l)
+        if match:
+            field, ver = match.groups()
+            ver_map[field] = ver
+
+    return '.'.join([ver_map['MAJOR'],
+                     ver_map['MINOR'],
+                     ver_map['BUGFIX']])
+
 setup(
     name = "asl_f4_loader",
-    version = "0.1.2",
+    version = extract_version(),
     author = "Jeff Ciesielski",
     author_email = "jeff@autosportlabs.com",
     description = ("A library and shell script to perform firmware updates on Cortex M4 Microcontrollers. (Currently: STM32F4)"),
@@ -20,7 +38,7 @@ setup(
     packages=['asl_f4_loader'],
     package_dir={'asl_f4_loader': 'host_tools',},
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Topic :: Utilities",
         "License :: OSI Approved :: GPLv2 License",
     ],
@@ -31,8 +49,9 @@ setup(
         ]
     },
     install_requires=[
-        'ihextools >= 1.0.0',
+        'ihextools >= 1.1.0',
         'pyserial >= 2.7',
         'crcmod >= 1.7',
+        'XBVC >= 0.0.1'
     ]
 )
---
+++ version.h
@@ -0,0 +1,8 @@
+#ifndef _VERSION_H_
+#define _VERSION_H_
+
+#define VER_MAJOR 0
+#define VER_MINOR 2
+#define VER_BUGFIX 0
+
+#endif
